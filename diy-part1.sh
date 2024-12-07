33e5ef765d45917c7a3285283fed1206457b8d31
onmytv <65473751+zhetengg@users.noreply.github.com>
2024-12-07 10:27:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,5 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #git clone https://github.com/vernesong/OpenClash.git
 #echo 'src-git vernesong https://github.com/vernesong/OpenClash' >>feeds.conf.defaulthttps://github.com/vernesong/OpenClash
