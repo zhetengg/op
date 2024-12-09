daee6806f3e5fd2d7c9fb6dee169b7042faf84f4
onmytv <65473751+zhetengg@users.noreply.github.com>
2024-12-09 10:32:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,12 +16,12 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
-#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-git clone https://github.com/kenzok8/openwrt-packages.git /package/openwrt-package
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 git clone https://github.com/kenzok8/small.git package/openwrt-small
 #git clone https://github.com/vi-si/luci-app-openclash.git package/luci-app-openclash
 #echo 'src-git vernesong https://github.com/vernesong/OpenClash' >>feeds.conf.default
 #echo 'src-git immortalwrt https://github.com/immortalwrt/homeproxy' >>feeds.conf.default
-#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+
