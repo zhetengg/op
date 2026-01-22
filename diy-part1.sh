a31fc15423c9c75bb89fe9c9f0e96fa2ca2ecefe
onmytv <65473751+zhetengg@users.noreply.github.com>
2026-01-22 17:03:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,5 +7,5 @@ sed -i '$a src-git openclash https://github.com/vernesong/OpenClash.git' feeds.c
 sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-# （可选）如果你想用特定作者的 Dockerman 优化版，可以取消下面这行的注释
+# （如果你想用特定作者的 Dockerman 优化版，可以取消下面这行的注释
 # sed -i '$a src-git dockerman https://github.com/lisaac/luci-app-dockerman.git' feeds.conf.default
