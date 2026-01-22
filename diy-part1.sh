ecb44cf4e81e86c78e00f82f333947bbeda8078f
onmytv <65473751+zhetengg@users.noreply.github.com>
2026-01-22 13:08:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-# 添加一些包含常见插件的仓库源
-echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+
+# 添加 OpenClash 官方源
+sed -i '$a src-git openclash https://github.com/vernesong/OpenClash.git' feeds.conf.default
+
+# 添加常用集成包（包含最新版 AdGuardHome 和一些增强插件）
+sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+# （可选）如果你想用特定作者的 Dockerman 优化版，可以取消下面这行的注释
+# sed -i '$a src-git dockerman https://github.com/lisaac/luci-app-dockerman.git' feeds.conf.default
