ece9dbe19550b82f1253358f6192718fde448e43
onmytv <65473751+zhetengg@users.noreply.github.com>
2026-01-22 13:05:54 +0800

    Modify default IP and add core plugin configurations
    Updated default IP address and ensured core plugins are selected.

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 1. 修改默认 IP 为 192.168.5.1 (按需修改)
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+# 2. 确保配置里选中了核心插件（防止手动选漏）
+echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-adguardhome=y" >> .config
