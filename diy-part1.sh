ef875d92063224fc2930559cc81a9138e2de79a4
onmytv <65473751+zhetengg@users.noreply.github.com>
2026-01-24 20:24:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# 添加 OpenClash 官方源
-sed -i '$a src-git openclash https://github.com/vernesong/OpenClash.git' feeds.conf.default
+echo "== DIY Part 1: feeds =="
 
-# 添加常用集成包（包含最新版 AdGuardHome 和一些增强插件）
-sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# OpenClash 官方源（保留）
+grep -q openclash feeds.conf.default || \
+  sed -i '$a src-git openclash https://github.com/vernesong/OpenClash.git' feeds.conf.default
 
-# （如果你想用特定作者的 Dockerman 优化版，可以取消下面这行的注释
+# ===== Actions 环境下，禁止 kenzok8 大源 =====
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+  echo "Running in GitHub Actions: skip kenzok8 feeds to save space"
+else
+  echo "Local build: enable kenzok8 feeds"
+  sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+  sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+fi
+
+# Dockerman 第三方源（默认关闭，避免重复）
 # sed -i '$a src-git dockerman https://github.com/lisaac/luci-app-dockerman.git' feeds.conf.default
+
+echo "feeds.conf.default:"
+cat feeds.conf.default
