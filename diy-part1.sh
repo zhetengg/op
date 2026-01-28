e995debdb28957ad773da557cbda29404fe796de
onmytv <65473751+zhetengg@users.noreply.github.com>
2026-01-28 09:55:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-# 禁止 git 交互（CI 必须）
+# 禁止 git 交互
 export GIT_TERMINAL_PROMPT=0
 
-# OpenClash（唯一额外 clone）
-git clone --depth=1 https://github.com/vernesong/OpenClash \
-  package/luci-app-openclash
+# 只 clone OpenClash
+git clone --depth=1 https://github.com/vernesong/OpenClash package/luci-app-openclash
