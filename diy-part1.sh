08a28db67265bb1321fc01d6beb83975d38d7b23
onmytv <65473751+zhetengg@users.noreply.github.com>
2026-01-28 08:07:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
+set -e
 
-# OpenClash
+# 禁止 git 交互（CI 必须）
+export GIT_TERMINAL_PROMPT=0
+
+# OpenClash（唯一额外 clone）
 git clone --depth=1 https://github.com/vernesong/OpenClash \
   package/luci-app-openclash
-
-# iStore 核心
-git clone --depth=1 https://github.com/linkease/istore \
-  package/istore
-
-# iStore UI
-git clone --depth=1 https://github.com/linkease/istore-ui \
-  package/istore-ui
-
-# 文件管理
-git clone --depth=1 https://github.com/linkease/luci-app-filebrowser \
-  package/luci-app-filebrowser
