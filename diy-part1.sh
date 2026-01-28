335eb0c4c3987e4111b5972c4d6783a1b6dae171
onmytv <65473751+zhetengg@users.noreply.github.com>
2026-01-28 13:24:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
-# 禁止 git 交互
 export GIT_TERMINAL_PROMPT=0
 
-# 只 clone OpenClash
+# OpenClash
 git clone --depth=1 https://github.com/vernesong/OpenClash package/luci-app-openclash
+
+# 修 util-linux GCC13 编译错误
+sed -i 's/-Werror//g' package/utils/util-linux/Makefile
